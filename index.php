2a599c0bc42a53cc4f16857fc720661572ba0ae0
barthola <up716038@myport.ac.uk>
2015-07-20 00:30:42 +0100

    Addition of scss animation
    Creation of a compass directory as well as addition of opening
    animations.

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta charset="UTF-8">
+    <meta name="description" content="Light Suits Ltd">
+    <meta name="author" content="Aleena Naeem">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0">
+    <link rel="icon" type="image/ico" href="images/favicon.ico"/>
+    <link rel="apple-touch-icon" href="images/favicon.ico"/>
+    <link rel="stylesheet" type="text/css" href="css/stylesheet.css"/>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+	<title>Light Suits</title>
+</head>
+<body>
+	<div id="no-zone">
+		<div id="borderContainer">
+			<div id="outerContainer">
+				<header>
+					<div id=""></div>
+					<img id="logo" src="images/samples/b3logo.png" alt="Light Suits Logo.">
+					<nav>
+						
+					</nav>
+				</header>
+				<div id="innerContainer">
+					<div id="jumbotron">
+						
+					</div>
+					<div id="about-us">
+						
+					</div>
+					<div id="contact-us"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
