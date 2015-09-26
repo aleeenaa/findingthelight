d956eadd6ccd242ab695583c4bf2c0399d72cd56
barthola <up716038@myport.ac.uk>
2015-09-26 18:26:59 +0100

    Finishing final draft and design

--- js/findingthelight.js
+++ js/findingthelight.js
@@ -1,6 +1,15 @@
+var balawal = function() {
+    $('#contactBtn').attr('href', 'mailto:joelight@live.co.uk?subject='
+    	+ encodeURIComponent("Customer Enquiry")+ "&body="
+    	+ encodeURIComponent("This is my body")
+    );
+};
+
 var eventListeners = function () {
 	var navigation = $('#navBar'),
-		header = $('#header');
+		header = $('#header'),
+		contact = $('#contactBtn'),
+		fb = $('#fbLogo');
 
 	header.mouseover(function() {
 		navigation.animate({
@@ -13,7 +22,15 @@ var eventListeners = function () {
 			});
 			// navigation.slideUp();
 		}, 10000)
-	})
+	});
+
+	fb.on('click', function() {
+		window.location.href = "https://www.facebook.com/pages/Light-Suits/789694201098958"
+	});
+
+	contact.on('click', function() {
+		$('#contactBtn a').click();
+	});
 }
 
 $(eventListeners);
